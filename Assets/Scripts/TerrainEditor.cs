69f46f90f061bec705065940e5798119d3207b91
Fbernabe1609 <fbernabe160903@gmail.com>
2024-05-11 23:04:24 +0200

    feat: edit terrain generator scripts

--- Assets/Scripts/TerrainEditor.cs
+++ Assets/Scripts/TerrainEditor.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class TerrainEditor : MonoBehaviour
 {
@@ -12,125 +15,113 @@ public class TerrainEditor : MonoBehaviour
     [SerializeField] private int detail = 150;
     [SerializeField] private float heightCorrection = -1;
     [SerializeField] private TerrainPaint paint;
-    [SerializeField] private int NumberTrees=5;
-    [SerializeField] private float minX=0f;
-    [SerializeField] private float maxX=1f;
-    [SerializeField] private float minZ=0f;
-    [SerializeField] private float maxZ=1f;
-    [SerializeField] private int waterHeight=160;
+    [SerializeField] private int numberOfTreesToPaint = 5;
+    [SerializeField] private float minX = 0f;
+    [SerializeField] private float maxX = 1f;
+    [SerializeField] private float minZ = 0f;
+    [SerializeField] private float maxZ = 1f;
+    [SerializeField] private int waterHeight = 160;
     [SerializeField] private GameObject water;
 
     private float[,] matrix;
 
-    public TMPro.TMP_InputField detailInput;
-    public TMPro.TMP_InputField waterHeightInput;
-    public TMPro.TMP_InputField seedInput;
-    public TMPro.TMP_InputField heightCorrectionInput;
+    public TMPro.TMP_InputField detailInput, waterHeightInput, seedInput, heightCorrectionInput;
 
     public Animator m_Animator;
 
-
-
-    // Start is called before the first frame update
     void Start()
     {
-
         matrix = new float[513, 513];
-        
         ShowPanel();
-        ActualizarTerreno();
-
+        UpdateTerrain();
     }
+
     public void ShowPanel()
     {
         detailInput.text = detail.ToString();
         seedInput.text = seed.ToString();
         waterHeightInput.text = waterHeight.ToString();
-        heightCorrectionInput.text = heightCorrection.ToString();
+        heightCorrectionInput.text = heightCorrection.ToString(CultureInfo.InvariantCulture);
     }
-    public void ActualizarTerreno()
+
+    public void UpdateTerrain()
     {
         try
         {
             detail = Int32.Parse(detailInput.text);
-        }
-        catch (FormatException e)
-        {
-        }
-        try
-        {
             seed = Int32.Parse(seedInput.text);
-        }
-        catch (FormatException e)
-        {
-        }
-        try
-        {
             CultureInfo culture = CultureInfo.CreateSpecificCulture("fr-FR");
-
-            heightCorrection = (float)Double.Parse(heightCorrectionInput.text,culture);
-        }
-        catch (FormatException e)
-        {
-        }
-        try
-        {
+            heightCorrection = (float)Double.Parse(heightCorrectionInput.text, culture);
             waterHeight = Int32.Parse(waterHeightInput.text);
         }
         catch (FormatException e)
         {
+            Debug.Log("Error: " + e.Message);
         }
+
         m_Animator.SetTrigger("Restart");
 
         HeightGenerator();
 
-        //Se pone la altura que será el número aleatorio creado
         terrainData.SetHeights(0, 0, matrix);
 
         paint.TerrainPainting();
 
-       // PaintTrees();
+        PaintTrees();
     }
 
+    // private void PaintTrees()
+    // {
+    //     for (int i = 0; i < NumberTrees; i++)
+    //     {
+    //         TreeInstance tree = new TreeInstance();
+    //         tree.prototypeIndex = 0;
+    //
+    //         //tree = terrainData.treeInstances[i]=tree;
+    //
+    //         Vector3 position = new Vector3(UnityEngine.Random.Range(minX, maxX), 0f,
+    //             UnityEngine.Random.Range(minZ, maxZ));
+    //         tree.position = position;
+    //
+    //         tree.position = new Vector3(1/ 100, 0, 1 / 100);
+    //         tree.prototypeIndex = 0;
+    //         tree.widthScale = 1f;
+    //         tree.heightScale = 1f;
+    //         tree.color = Color.white;
+    //         tree.lightmapColor = Color.white;
+    //
+    //         terrain.AddTreeInstance(tree);
+    //     }
+    // }
+    
     private void PaintTrees()
     {
-        for (int i = 0; i < NumberTrees; i++)
+        for (int i = 0; i < numberOfTreesToPaint; i++)
         {
-            //Instanciar árbol
-            TreeInstance tree= new TreeInstance();
-            tree.prototypeIndex =0;
-
-            //tree = terrainData.treeInstances[i]=tree;
-
-            //Colocarlo aleatoriamente dentro de los márgenes
-            Vector3 position = new Vector3(UnityEngine.Random.Range(minX, maxX), 0f, UnityEngine.Random.Range(minZ,maxZ));
+            TreeInstance tree = new TreeInstance();
+            tree = terrainData.treeInstances[i];
+            Vector3 position = new Vector3(Random.Range(minX,maxX), 0, Random.Range(minZ,maxZ));
             tree.position = position;
-
-            /*tree.position = new Vector3(1/ 100, 0, 1 / 100);
-            tree.prototypeIndex = 0;
-            tree.widthScale = 1f;
-            tree.heightScale = 1f;
-            tree.color = Color.white;
-            tree.lightmapColor = Color.white;*/
-
-            //Añadirlo al terreni
             terrain.AddTreeInstance(tree);
         }
     }
-
+    
     private void HeightGenerator()
     {
-        water.transform.position= new Vector3(water.transform.position.x,waterHeight,water.transform.position.z);
-        for(int i=0; i<matrix.GetLength(0); i++)
+        water.transform.position = new Vector3(water.transform.position.x, waterHeight, water.transform.position.z);
+
+        float seedDetailRatio = (float)seed / detail;
+        int matrixLength0 = matrix.GetLength(0);
+        int matrixLength1 = matrix.GetLength(1);
+
+        Parallel.For(0, matrixLength0, i =>
         {
-            for( int j=0; j<matrix.GetLength(1); j++)
+            float iDetailRatio = (float)i / detail;
+            for (int j = 0; j < matrixLength1; j++)
             {
-                //Cada parte se le da un número aleatorio
-                // matrix[i, j] = UnityEngine.Random.Range(0.0f, 0.005f);
-
-                matrix[i, j] = Mathf.PerlinNoise((float)(i+seed)/detail, (float)(j+seed)/detail)+heightCorrection;
+                matrix[i, j] = Mathf.PerlinNoise(iDetailRatio + seedDetailRatio, (float)j / detail + seedDetailRatio) +
+                               heightCorrection;
             }
-        }        
+        });
     }
-
 }
--- Assets/Scripts/TerrainPaint.cs
+++ Assets/Scripts/TerrainPaint.cs
@@ -10,40 +10,34 @@ public class TerrainPaint : MonoBehaviour
     }
 
     public HeightTexture[] heightTexture;
-
-
+    
     public void TerrainPainting()
     {
         TerrainData terrainData = Terrain.activeTerrain.terrainData;
         float[,,] mapData = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+        float[] paint = new float[heightTexture.Length];
 
         for (int x = 0; x < terrainData.alphamapWidth; x++)
         {
             for (int y = 0; y < terrainData.alphamapHeight; y++)
             {
                 float localHeight = terrainData.GetHeight(y, x);
-                float[] paint = new float[heightTexture.Length];
-
-                for (int i = 0; i < heightTexture.Length-1; i++)
-                {
-                    if (localHeight >= heightTexture[i].initHeight && localHeight < heightTexture[i+1].initHeight)
-                    {
-                        paint[i] = 1;
-                    }
-                }
-                if (localHeight >= heightTexture[heightTexture.Length-1].initHeight)
-                {
-                    paint[heightTexture.Length-1] = 1;
-                }
 
                 for (int i = 0; i < heightTexture.Length; i++)
                 {
+                    paint[i] = 0;
+                    if (i < heightTexture.Length - 1 && localHeight >= heightTexture[i].initHeight && localHeight < heightTexture[i+1].initHeight)
+                    {
+                        paint[i] = 1;
+                    }
+                    else if (localHeight >= heightTexture[heightTexture.Length-1].initHeight)
+                    {
+                        paint[heightTexture.Length-1] = 1;
+                    }
                     mapData[x, y, i] = paint[i];
                 }
             }
         }
-
         terrainData.SetAlphamaps(0, 0, mapData);
     }
-
 }
